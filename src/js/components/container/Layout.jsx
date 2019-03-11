befc254c432e2fd7f2fa1b92c28e26ae210bdcec
Daniel Dvorkin <daniel.dvorkin@ptaginc.com>
2019-03-11 15:40:38 -0400

    first step of state

--- src/js/components/container/Layout.jsx
+++ src/js/components/container/Layout.jsx
@@ -9,11 +9,27 @@ import Welcome from '../presentational/Welcome.jsx';
 // import Input from "../presentational/Input.jsx";
 
 class Layout extends Component {
+  constructor(){
+    super();
+    this.state = {
+      firstName: 'Daniel',
+      clicked: 0
+    }
+  }
+
+  increaseClicked(){
+    let new_count = this.state.clicked + 1
+
+    this.setState({
+      clicked: new_count
+    })
+  }
+
   render() {
     return (
-      <div>
-        <NavBar />
-        <Welcome title="Amazing new React/Webpack app" msg="created with love <3"/>
+      <div onClick={this.increaseClicked.bind(this)}>
+        <h1>Hello {this.state.firstName}</h1>
+        <p>You've clicked hello {this.state.clicked} times</p>
       </div>
     );
   }
